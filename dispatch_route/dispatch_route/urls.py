416e77241080f491063d7b6372464dd2962a066e
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-10-17 12:04:50 +0300

    Delete dispatch_route directory

--- dispatch_route/dispatch_route/urls.py
+++
@@ -1,8 +0,0 @@
-# dispatch_system/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('dispatch.urls')),
-]
